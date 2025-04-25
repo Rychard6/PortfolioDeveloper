47b081dc1f2964ae7c7bb2555057c6a20497dab4
Rychard6 <rychardryan6@gmail.com>
2025-04-25 16:41:44 -0300

    New Curriculo with sitemap

--- src/app/sitemap.sml/route.ts
+++ src/app/sitemap.sml/route.ts
@@ -1,6 +1,4 @@
-import { GetServerSideProps } from "next";
-
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
@@ -9,17 +7,12 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
         <changefreq>monthly</changefreq>
         <priority>1.0</priority>
       </url>
-    </urlset>`;
+    </urlset>
+  `.trim();
 
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
-
-  return {
-    props: {},
-  };
-};
-
-export default function Sitemap() {
-  return null;
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
